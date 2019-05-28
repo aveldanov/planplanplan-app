a9fc134ab7f7dd24f2879bfaa934dd0c29edab38
Anton Veldanov <anton.veldanov@gmail.com>
2019-05-28 17:52:50 +1000

    new

---
+++ src/config/fbConfig.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBuHatpCHx9MWp5vyg4sUWJSHUyfeQoxtM",
+  authDomain: "planplanplan-86871.firebaseapp.com",
+  databaseURL: "https://planplanplan-86871.firebaseio.com",
+  projectId: "planplanplan-86871",
+  storageBucket: "planplanplan-86871.appspot.com",
+  messagingSenderId: "52847950569",
+  appId: "1:52847950569:web:424ed85e6416dc9b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
